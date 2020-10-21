7101488bbf14912e8f8bd43481dbbdea3ff6df80
mirasoil <69077409+mirasoil@users.noreply.github.com>
2020-10-21 11:05:53 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ispas_Teodora_Lab2_modificat
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
